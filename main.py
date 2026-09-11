90bcb6750f9e5158b2a039997db2fe7946c84842
Aakash <aakash@Aakashs-MacBook-Air.local>
2026-09-11 16:06:43 +0530

    base route /docs

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from app.routes.issues import router as issues_router
 from app.middleware.timer import timing_middleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 app=FastAPI()
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 app.middleware("http")(timing_middleware)
 app.add_middleware(
     CORSMiddleware,
